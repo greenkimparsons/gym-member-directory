4fba3a3d9facb549517934286a1f4079577b937e
Green Kim <greenkimparsons@gmail.com>
2021-05-19 09:14:34 -0400

    added Body, Search, and Table.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,19 +3,14 @@ import API from "../utils/API";
 import Table from "./Table";
 
 export default class Body extends Component {
-  // I am now considering the Body to be the parent component of my children later on.
   state = {
     users: [],
     filteredUsers: [],
   };
 
   componentDidMount() {
-    // Mount the data to run after this component renders to the page
     API.getUsers().then((res) => {
-      // Mapped through it because this is the value I wanted to hold, not really needed at this point honestly.
-      const holdData = res.data.results.map((user) => {
-        return user;
-      });
+      const holdData = res.data.results.map((user) => {});
       this.setState({
         users: holdData,
       });
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,19 +2,18 @@ import React from "react";
 
 function Table(props) {
   console.log(props);
-  // The data was coming in with the error because I didn't wrap it in JSX.
   const userData = props.users.map((user) => {
     return <div>{user.name.first}</div>;
   });
   console.log("This is the userData after returning as a div", userData);
   return (
-    <div className="container">
-      {userData}
-      {/* You can run the function above inside the return as a jsx expression or do a function call above  */}
-      {/* {props.users.map((user) => {
-				return <div>{user.name.first}</div>;
-			})} */}
-    </div>
+      <div className="container">
+          {userData}
+          {/* You can run the fucntino above inside the return as a jsx expression or do a function call above */}
+          {/* {props.users.map((user) => {
+              return <div>{user.name.first}</div>;
+          })} */}
+      </div>
   );
 }
 
